94405c1b5e7c580b88a77b1e022f7f2a4d290da1
Alejodev <alejod2103@gmail.com>
2022-05-13 15:06:46 -0600

    el error no era la seccion de contenido exclusivo xd

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,6 +3,7 @@ import { Content } from '../components/Content/Content';
 import Features from '../components/Features/Features';
 import Hero from '../components/Hero/Hero';
 import { heroOne, heroTwo, heroThree } from '../data/HeroData';
+import Contenttwo from '../components/Content/Contents/ContentTwo';
 import Compatibles from './Compatibles';
 // Hero Feature Content Carousel
 
@@ -15,7 +16,7 @@ const Home = () => {
 			<Content {...heroOne} />
 			<Content {...heroTwo} />
 			<Content {...heroThree} />
-			
+			<Contenttwo />
 			<Compatibles />
 		</>
 	);

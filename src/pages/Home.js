3ee439e1a6e3105ebf8956b52441aec0354ece3a
Alejodev <alejod2103@gmail.com>
2022-05-13 15:21:16 -0600

    yase el warning

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Content } from '../components/Content/Content';
 import Features from '../components/Features/Features';
-import Hero from '../components/Hero/Hero';
 import { heroOne, heroTwo, heroThree } from '../data/HeroData';
 import Contenttwo from '../components/Content/Contents/ContentTwo';
 import Compatibles from './Compatibles';

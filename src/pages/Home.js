3cec93c43017b1a3dde017d9d80795f1ff273926
Alejodev <alejod2103@gmail.com>
2022-05-13 15:03:31 -0600

    solucion de arnings

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,7 +3,6 @@ import { Content } from '../components/Content/Content';
 import Features from '../components/Features/Features';
 import Hero from '../components/Hero/Hero';
 import { heroOne, heroTwo, heroThree } from '../data/HeroData';
-import Contenttwo from '../components/Content/Contents/ContentTwo';
 import Compatibles from './Compatibles';
 // Hero Feature Content Carousel
 
